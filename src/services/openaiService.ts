3520466482ceed586ebb58460402bc9648b58ee8
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-11-14 16:45:15 -0300

    Criando conexão com api

--- src/services/openaiService.ts
+++ src/services/openaiService.ts
@@ -1,15 +1,54 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: <explanation> */
+import OpenAI from "openai";
+
+//Instância cliente
+const client = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+//Transcrição de áudio
 export const openaiTranscribeAudio = async (
-	_file: Express.Multer.File,
-): Promise<void> => {
-	// Lógica para transcrever o áudio usando OpenAI
+	file: Express.Multer.File,
+): Promise<any> => {
+	const response = await client.audio.transcriptions.create({
+		file: file.buffer,
+		model: "gpt-4o-audio",
+	});
+
+	return response.text;
 };
 
-export const openaiTranscribeText = async (_text: string): Promise<void> => {
-	// Lógica para transcrever o texto usando OpenAI
+//Apenas retorna o próprio texto ou opcionalmente "melhora"
+export const openaiTranscribeText = async (
+	text: string,
+): Promise<Promise<any>> => {
+	return text;
 };
 
+//Gera diagnóstico baseado na transcrição completa
 export const openaiGenerateDiagnosis = async (
-	_transcript: string,
-): Promise<void> => {
-	// Lógica para gerar diagnóstico usando OpenAI
+	transcript: string,
+): Promise<Promise<any>> => {
+	const prompt = `
+Você é um médico assistente.  
+Com base na conversa abaixo, gere:
+
+1. Diagnóstico provável  
+2. Lista de doenças associadas  
+3. Lista de exames recomendados  
+4. Lista de medicamentos possíveis  
+
+Transcrição:
+"${transcript}"
+`;
+
+	const response = await client.chat.completions.create({
+		model: "gpt-4o-mini",
+		messages: [{ role: "user", content: prompt }],
+		temperature: 0.2,
+	});
+
+	const content = response.choices[0]?.message.content;
+
+	return { result: content };
 };
